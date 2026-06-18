9d3d72351c696ffe8cdb8078a4c8a11974f5f8f5
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-18 23:14:54 +0530

    Basic

---
+++ Basic/py.py
@@ -0,0 +1,17 @@
+print("23")
+print("23" + "23" == "2323")
+
+name = "John"
+age = 30
+print("Hello, " + name + "! You are " + str(age) + " years old.")
+
+
+print(type(name))
+print(type(age))
+print(type(3.14))
+x = 10
+y = 3.14
+print(type(x))
+print(type(y))
+
+
