ea580196b67a0f40918c413c2216f89063635c7b
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-18 23:20:55 +0530

    Basic

---
+++ Basic/add.py
@@ -0,0 +1,23 @@
+#addition
+x = 5
+y = 3
+z = x + y
+print(z)
+
+#subtraction
+x = 5
+y = 3
+z = x - y
+print(z)
+
+#multiplication
+x = 5
+y = 3
+z = x * y
+print(z)
+
+#division
+x = 5
+y = 3
+z = x / y
+print(z)
