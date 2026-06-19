fd0a0502c2f1a83dd8b5d8b226447c45b4e8445f
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-19 23:56:04 +0530

    Basic

---
+++ Basic/conditinal.py
@@ -0,0 +1,26 @@
+# Conditional Statements in Python
+
+x = 10
+y = 20
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+
+
+
+
+
+x = 15
+y = 15  
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
+    
