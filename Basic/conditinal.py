91c06af2976732b1d09613e515e8c74a93c5e1ab
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-19 23:59:15 +0530

    Basic

--- Basic/conditinal.py
+++ Basic/conditinal.py
@@ -23,4 +23,37 @@ elif x < y:
     print("x is less than y")
 else:
     print("x is equal to y")
-    
+
+
+#if-else statement
+age = 16
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote")
+
+
+#if-elif-else Statement   
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+#Nested if
+age = 20
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("Entry Allowed")
+    else:
+        print("ID Required")
+else:
+    print("Under Age")
