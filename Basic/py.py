f020333dc4308676fdbc83357afebecbb89411b2
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-21 20:31:00 +0530

    Basic

--- Basic/py.py
+++ Basic/py.py
@@ -36,3 +36,37 @@ print(coordinates)
 person = {"name": "John", "age": 30}
 print(person)
 
+a = 10
+b = "Priyanshu"
+c = 3.14
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+A,B = 3,2
+C=4.0
+txt = "Priyanshu "
+print(A*B*txt)
+print(A*(B*txt))  #CONCATENATION
+print(A+B*C)
+print((A+B)*C)
+
+greeting = "Hello, World!"
+print(greeting[0])  # H 
+print(greeting[1])  # W
+print(greeting[-1]) # !
+print(greeting[0:5]) # Hello
+print(greeting[7:])  # World!
+print(greeting[:5])  # Hello
+print(greeting[::2]) # Hlo ol!
+print(greeting[::-1]) # !dlroW ,olleH
+print(greeting.upper()) # HELLO, WORLD!
+print(greeting.lower()) # hello, world!
+print(greeting.replace("World", "Python")) # Hello, Python!
+print(greeting.split(", ")) # ['Hello', 'World!']
+print(greeting.strip("!")) # Hello, World
+print(greeting.find("World")) # 7
+print(greeting.count("o")) # 2
--- Basic/tempCodeRunnerFile.py
+++ Basic/tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
 
-# Taking multiple inputs in a single line
-numbers = input("Enter three numbers separated by spaces: ")
-num1, num2, num3 = map(int, numbers.split())
-print(f"The three numbers are: {num1}, {num2}, and {num3}")
+A,B = 3,2
+C=4.0
+txt = "Priyanshu "
+print(A*B*txt)
+print(A*(B*txt))  #CONCATENATION
+print(A+B*C)
+print((A+B)*C)
