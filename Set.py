ad7525c26010e55153fa103b2a0bebdcdc0280d2
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-06-28 23:52:49 +0530

    set

---
+++ Set.py
@@ -0,0 +1,20 @@
+#A set in Python is a collection that stores unique (no duplicates) and unordered elements.
+
+# Empty set
+s = set()
+print(s)
+
+# Set with values
+fruits = {"apple", "banana", "mango", "5"}
+print(fruits)
+print(type(fruits))
+print(len(fruits))
+
+
+#duplicate value
+numbers = {1, 2, 3, 2, 1, 4}
+print(numbers)
+print(len(numbers))
+
+
+#method in set
