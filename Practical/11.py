8c5e855920336dea4f3c3c78a1067dec5ede8c56
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-09-18 11:30:09 +0530

    11th practical

---
+++ Practical/11.py
@@ -0,0 +1,12 @@
+#Write a Python script that prints prime numbers less than 20. 
+
+for num in range(2, 20):
+    is_prime = True
+
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, end=" ")
