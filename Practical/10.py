0fab3937c4244965ea1d378ecd8219e1110e03d9
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-09-18 11:28:42 +0530

    10th practical

---
+++ Practical/10.py
@@ -0,0 +1,12 @@
+# Write a Python program to construct the following pattern, using a nested for loop 
+
+
+for i in range(1, 5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(3, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
