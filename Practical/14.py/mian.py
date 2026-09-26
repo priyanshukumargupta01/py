0a84c46d3537fd2404aeabffbfaed02387c43b49
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-09-26 23:19:20 +0530

    14th practical(module)

---
+++ Practical/14.py/mian.py
@@ -0,0 +1,9 @@
+# Importing the fibonacci module
+import fibonacci
+
+# Taking input
+n = int(input("Enter the number of terms: "))
+
+# Calling the function from the module
+print("Fibonacci Series:")
+fibonacci.fibonacci(n)
